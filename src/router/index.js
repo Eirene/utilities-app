56f97f1f1808529e06eeb7ea4f8739be5ccb5158
Eirene <sorokina333@gmail.com>
2022-02-16 02:05:21 +0300

    remove Cubbies

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import PageNotFound from "../views/PageNotFound.vue";
 import Home from "../views/Home.vue";
 import NotifyPage from "../views/NotifyPage.vue";
-import Cubbies from "../views/Cubbies.vue";
 import TestPage from "../views/TestPage.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home, },
   { path: "/notify", name: "Notify", component: NotifyPage, },
   { path: "/test", name: "Test", component: TestPage, },
-  { path: "/cubbies", name: "Cubbies", component: Cubbies, },
   { path: "/:notFound(.*)", name: "PageNotFound", component: PageNotFound, },
 ];
 
