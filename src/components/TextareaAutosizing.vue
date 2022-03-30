6c423c0c2c67d8d4d6c43a76aa10e6fe3eee91db
Eirene <sorokina333@gmail.com>
2022-03-30 23:17:37 +0300

    feat: add TextareaAutosizing.vue

---
+++ src/components/TextareaAutosizing.vue
@@ -0,0 +1,25 @@
+<script setup>
+const resize = (e) => {
+  e.target.style.height = 'auto'
+  e.target.style.height = `${e.target.scrollHeight}px`
+}
+
+defineProps({
+  modelValue: String,
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div>
+    <label for="comment-autoresize" class="block text-sm font-medium text-gray-700">
+      Add your text to see how textarea will be automatically resize
+    </label>
+    <div class="mt-1">
+      <textarea rows="4" name="comment-autoresize" id="comment-autoresize"
+                @input="$emit('update:modelValue', $event.target.value); resize($event)"
+                :value="modelValue"
+                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,9 @@ import Collections2Wallet from "../components/Collections2Wallet.vue";
 import MultiSelect from "../components/MultiSelect.vue";
 import GithubSearch from "../components/GithubSearch.vue";
 import GrandForm from "../components/GrandForm.vue";
+import TextareaAutosizing from "../components/TextareaAutosizing.vue";
 
+const textareaAutosizing = ref('textareaAutosizing');
 const multiSelect = ref(null);
 const multiSelectOptions = ["Batman", "Robin", "Joker"];
 
@@ -21,4 +23,9 @@ defineExpose({
   <multi-select v-model="multiSelect" :items="multiSelectOptions"></multi-select>
   <file-upload />
   <grand-form />
+
+  <div class="max-w-lg mx-auto mb-12">
+    <textarea-autosizing v-model="textareaAutosizing" />
+    <p>Your text: {{ textareaAutosizing }}</p>
+  </div>
 </template>
