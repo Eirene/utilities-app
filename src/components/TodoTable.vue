41f20e42e3c0d24ece0246e65c2d11daa62c1a03
Eirene <sorokina333@gmail.com>
2022-04-09 22:05:47 +0300

    pages: updates

--- src/components/TodoTable.vue
+++ src/components/TodoTable.vue
@@ -13,15 +13,15 @@ defineProps({
       <thead class="bg-gray-50">
       <tr>
         <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Id</th>
-        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">UserId</th>
+        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">UserId</th>
         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
         <th scope="col" class="py-3.5 pr-4 pl-3 text-center text-sm font-semibold text-gray-900">Completed</th>
       </tr>
       </thead>
       <tbody class="bg-white">
       <tr v-for="todo in todoList" :key="todo.id" :class="todo.id % 2 === 0 ? undefined : 'bg-gray-50'">
-        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ todo.id }}</td>
-        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ todo.userId }}</td>
+        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-6">{{ todo.id }}</td>
+        <td class="whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">{{ todo.userId }}</td>
         <td class="px-3 py-4 text-left text-sm text-gray-500">{{ todo.title }}</td>
         <td class="whitespace-nowrap py-4 pr-4 pl-3 text-sm text-gray-500 text-center">
           <CheckCircleIcon v-if="todo.completed"
--- src/views/PagePagination.vue
+++ src/views/PagePagination.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, reactive, computed, watch } from 'vue';
+import { ref, onMounted, reactive, computed } from 'vue';
 import { useStore } from "vuex";
 import axios from "axios";
 import AppPagination from "../components/AppPagination.vue";
@@ -12,28 +12,26 @@ const todoListFiltered = computed(() => store.state.todo.todoFiltered);
 const meta = reactive({
   current_page: 1,
   from: 1,
-  to: 5,
-  per_page: 5,
+  to: 7,
+  per_page: 7,
   total: 5,
 });
 
-meta.last_page = meta.total / meta.per_page;
+meta.last_page = Math.ceil(meta.total / meta.per_page);
 
-const filterList = ((list) => {
-  return list.filter((item => item.id >= meta.from && item.id <= meta.to))
-})
+const filterList = (list => list.filter((item => item.id >= meta.from && item.id <= meta.to)));
 
 const loadTodoList = (() => {
   loading.value = true;
   axios
-    // .get("https://jsonplaceholder.typicode.com/todos?userId=1")
-    .get("https://jsonplaceholder.typicode.com/todos")
+    .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+    // .get("https://jsonplaceholder.typicode.com/todos")
     .then((response) => {
       let listAll = response.data;
       let listFiltered = filterList(listAll);
 
       meta.total = listAll.length;
-      meta.last_page = listAll.length/meta.per_page;
+      meta.last_page = Math.ceil(listAll.length/meta.per_page);
 
       store.dispatch('setTodo', listAll);
       store.dispatch('setTodoFiltered', listFiltered);
@@ -49,12 +47,9 @@ const loadTodoList = (() => {
 const getTodoList = ((page = 1) => {
   if (page <= 0 || page > meta.last_page) return
   meta.current_page = page;
-  store.dispatch('setTodoFiltered', filterList(store.state.todo.todo));
-})
-
-watch(meta, () => {
   meta.from = (meta.per_page * (meta.current_page - 1)) + 1;
-  meta.to = (meta.from + meta.per_page) - 1;
+  meta.to = (meta.from + meta.per_page - 1 > meta.total) ? meta.total : meta.from + meta.per_page - 1;
+  store.dispatch('setTodoFiltered', filterList(store.state.todo.todo));
 })
 
 onMounted(() => {
