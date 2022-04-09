1f0b629ba0da82457da77048e44705c5cb798294
Eirene <sorokina333@gmail.com>
2022-04-09 23:36:32 +0300

    pages: updates

--- src/views/PagePagination.vue
+++ src/views/PagePagination.vue
@@ -4,20 +4,22 @@ import { useStore } from "vuex";
 import axios from "axios";
 import AppPagination from "../components/AppPagination.vue";
 import TodoTable from "../components/TodoTable.vue";
+import { useRoute, useRouter } from "vue-router";
 
 const store = useStore();
+const router = useRouter();
+const route = useRoute();
 const loading = ref(false);
 const todoListFiltered = computed(() => store.state.todo.todoFiltered);
 
 const meta = reactive({
-  current_page: 1,
-  from: 1,
-  to: 7,
+  current_page: Number(route.query.page) || 1,
   per_page: 7,
-  total: 5,
+  total: null,
 });
 
-meta.last_page = Math.ceil(meta.total / meta.per_page);
+meta.from = (meta.current_page - 1) * meta.per_page + 1;
+meta.to = meta.from + meta.per_page - 1;
 
 const filterList = (list => list.filter((item => item.id >= meta.from && item.id <= meta.to)));
 
@@ -44,8 +46,13 @@ const loadTodoList = (() => {
     });
 });
 
-const getTodoList = ((page = 1) => {
+const getTodoList = ((page = Number(route.query.page)) => {
   if (page <= 0 || page > meta.last_page) return
+  router.replace({
+    query: {
+      page: page
+    }
+  });
   meta.current_page = page;
   meta.from = (meta.per_page * (meta.current_page - 1)) + 1;
   meta.to = (meta.from + meta.per_page - 1 > meta.total) ? meta.total : meta.from + meta.per_page - 1;
