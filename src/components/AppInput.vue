c73c8cd064e5337bc1fb652979f51d302ecb88b3
Eirene <sorokina333@gmail.com>
2022-08-27 13:36:39 +0300

    feat: update AppInput

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,12 +1,26 @@
 <script setup>
-
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  placeholder: String,
+  type: {
+    type: String,
+    default: 'text',
+  },
+})
+defineEmits(['update:modelValue'])
 </script>
 
 <template>
-  <label>Email</label>
-  <div class="mt-1 relative rounded-md shadow-sm">
-    <input placeholder="test@test.com" class="h-10 tailwind-input" />
+  <div class="relative">
+    <input :type="type"
+           :placeholder="placeholder"
+           @input="$emit('update:modelValue', $event.target.value)"
+           :value="modelValue"
+           class="tailwind-input" />
+    <slot name="icon"></slot>
   </div>
-  <slot></slot>
-  <p class="mt-2 text-sm text-red-600">Text error</p>
+  <slot name="helptext"></slot>
 </template>
--- src/components/GrandForm.vue
+++ src/components/GrandForm.vue
@@ -1,24 +1,29 @@
 <script setup>
+import { ref } from 'vue';
 import { CheckCircleIcon } from "@heroicons/vue/solid";
 import AppInput from "./AppInput.vue";
 
+const grandForm = ref('');
 </script>
 
 <template>
-  <p class="flex items-center justify-center text-sm mb-2">
-    <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
-    This form uses Component with VeeValidate and Maska
-  </p>
-  <p class="flex items-center justify-center text-sm mb-4">
-    <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
-    You can click on the hints to fill out faster
-  </p>
-
+<!--  <p class="flex items-center justify-center text-sm mb-2">-->
+<!--    <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />-->
+<!--    This form uses Component with VeeValidate and Maska-->
+<!--  </p>-->
+<!--  <p class="flex items-center justify-center text-sm mb-4">-->
+<!--    <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />-->
+<!--    You can click on the hints to fill out faster-->
+<!--  </p>-->
   <div class="max-w-md mx-auto mb-12 p-8 bg-cyan-100 rounded-2xl text-cyan-900">
     <form action="">
-      <app-input>
-        <p class="mx-3 inline-block text-sm text-gray-500
-                  hover:text-gray-800 cursor-pointer border-b border-dashed">test@test.com</p>
+      <p class="pl-3">v-model: {{grandForm}}</p>
+      <app-input v-model="grandForm" :type="'text'" :placeholder="'Placeholder text'">
+        <template #header>
+          <p class="mx-3 inline-block text-sm text-gray-500
+                    hover:text-gray-800 cursor-pointer border-b border-dashed"
+            @click.prevent="grandForm = 'test@test.com'">test@test.com</p>
+        </template>
       </app-input>
     </form>
   </div>
